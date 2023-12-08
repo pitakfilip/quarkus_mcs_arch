0e7af7cb8533fab8c47971ba929f7c88460a56b4
Filip Pitak <fpitak11@gmail.com>
2023-12-08 19:02:10 +0100

    Kingdom import.sql update with domain users

--- kingdom/kingdom-application/src/main/resources/import.sql
+++ kingdom/kingdom-application/src/main/resources/import.sql
@@ -6,3 +6,9 @@ insert into kingdom (name, level, experience) values('TUTUTU TU Max Verstappen '
 insert into kingdom (name, level, experience) values('Egove mesto snov', 2, 69);
 insert into kingdom (name, level, experience) values('Lorem Ipsum', 30, 150);
 insert into kingdom (name, level, experience) values('[TAA] Trenbolone Acetate Abusers', 19, 55);
+
+insert into domainuser (id, kingdomid, name, password, username) values (1, 1, 'Jozo', 'a', 'Jozo');
+insert into domainuser (id, kingdomid, name, password, username) values (2, 2, 'MaxVerstappen', 'a', 'MaxVerstappen');
+insert into domainuser (id, kingdomid, name, password, username) values (3, 3, 'EGOJEBO', 'a', 'EGOJEBO');
+insert into domainuser (id, kingdomid, name, password, username) values (4, 4, 'ipsumLorem', 'a', 'ipsumLorem');
+insert into domainuser (id, kingdomid, name, password, username) values (5, 5, 'Cbum', 'a', 'Cbum');
