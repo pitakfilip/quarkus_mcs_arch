afad588415980698973322dd1635904d3a5d6cda
Filip Pitak <fpitak11@gmail.com>
2023-12-10 23:57:11 +0100

    Fix warfare proxy path value

--- kingdom/kingdom-application/src/main/java/cz/muni/soa/kingdom/proxy/WarfareProxy.java
+++ kingdom/kingdom-application/src/main/java/cz/muni/soa/kingdom/proxy/WarfareProxy.java
@@ -6,7 +6,7 @@ import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@Path("/shop")
+@Path("/warfare")
 @RegisterClientHeaders(ServiceAuthHeaderPropagation.class)
 @RegisterRestClient(baseUri = "stork://warfare")
 public interface WarfareProxy extends WarfareApi {
